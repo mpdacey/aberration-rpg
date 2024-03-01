49fc5af8eb9b17a3a42814f558d5f784be760be0
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-02 10:59:40 +1100

    Added attributes to combatants.

--- Assets/Scripts/ScriptableObjectSources/CombatantScriptableObject.cs
+++ Assets/Scripts/ScriptableObjectSources/CombatantScriptableObject.cs
@@ -14,10 +14,30 @@ public class CombatantScriptableObject : ScriptableObject
         public int luck;
     }
 
+    public struct CombatantAttributes
+    {
+        public AttributeAffinity physical;
+        public AttributeAffinity fire;
+        public AttributeAffinity ice;
+        public AttributeAffinity thunder;
+        public AttributeAffinity wind;
+    }
+
+    public enum AttributeAffinity
+    {
+        None,
+        Weak,
+        Resist,
+        Null,
+        Repel,
+        Absorb
+    }
+
     public string combatantName;
     public int combatantMaxHealth;
     public int combatantMaxStamina;
     public CombatantStats combatantBaseStats;
+    public CombatantAttributes combatantAttributes;
     public List<SpellScriptableObject> combatantSpells = new List<SpellScriptableObject>();
     public DiceScriptableObject combatantDice;
     public Sprite combatantSprite;
