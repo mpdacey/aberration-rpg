610f7152a6e0a753a440f74102a32bd87956f449
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-02 18:59:01 +1100

    Attributes now use a dictionary
    The key for the dictionary is the spell type and the value is the affinity to that type.
    
    I had to make an array of structs so that I could input the values for the dictionary in the inspector.
    
    Unity should really add dictionary support for the inspector natively.

--- Assets/Scripts/ScriptableObjectSources/CombatantScriptableObject.cs
+++ Assets/Scripts/ScriptableObjectSources/CombatantScriptableObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Monster", menuName = "Scriptable Objects/Combatant")]
 public class CombatantScriptableObject : ScriptableObject
 {
+    [System.Serializable]
     public struct CombatantStats
     {
         public int strength;
@@ -12,16 +14,6 @@ public class CombatantScriptableObject : ScriptableObject
         public int endurance;
         public int agility;
         public int luck;
-    }
-
-    public struct CombatantAttributes
-    {
-        public AttributeAffinity blunt;
-        public AttributeAffinity sharp;
-        public AttributeAffinity fire;
-        public AttributeAffinity ice;
-        public AttributeAffinity thunder;
-        public AttributeAffinity wind;
     }
 
     public enum AttributeAffinity
@@ -38,8 +30,33 @@ public class CombatantScriptableObject : ScriptableObject
     public int combatantMaxHealth;
     public int combatantMaxStamina;
     public CombatantStats combatantBaseStats;
-    public CombatantAttributes combatantAttributes;
+    public Dictionary<SpellScriptableObject.SpellType, AttributeAffinity> combatantAttributes = new Dictionary<SpellScriptableObject.SpellType, AttributeAffinity>();
     public List<SpellScriptableObject> combatantSpells = new List<SpellScriptableObject>();
+    public SpellScriptableObject.SpellType combatantNormalAttackType;
     public DiceScriptableObject combatantDice;
     public Sprite combatantSprite;
+
+    [System.Serializable]
+    private struct AttributeAffinityDictionaryItem
+    {
+        public SpellScriptableObject.SpellType key;
+        public AttributeAffinity value;
+    }
+
+    [SerializeField]
+    private AttributeAffinityDictionaryItem[] combatantAttributesArray;
+
+    private void OnValidate()
+    {
+        if (combatantAttributesArray.Length != Enum.GetNames(typeof(SpellScriptableObject.SpellType)).Length)
+            combatantAttributesArray = new AttributeAffinityDictionaryItem[Enum.GetValues(typeof(SpellScriptableObject.SpellType)).Length];
+
+        int i = 0;
+        foreach (SpellScriptableObject.SpellType key in Enum.GetValues(typeof(SpellScriptableObject.SpellType)))
+        {
+            combatantAttributesArray[i].key = key;
+            combatantAttributes[key] = combatantAttributesArray[i].value;
+            i++;
+        }
+    }
 }
