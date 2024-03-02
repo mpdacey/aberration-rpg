7ff0681451b89eeb95ebdc60f517afe4157b189d
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-02 19:04:07 +1100

    Created DeliverAttack method
    The deliver attack method now takes an incoming attack object and calculates the damage values for which the monster will receive the damage.

--- Assets/Scripts/MonsterController.cs
+++ Assets/Scripts/MonsterController.cs
@@ -62,9 +62,55 @@ public class MonsterController : MonoBehaviour
         return attack;
     }
 
+    public AttackObject DeliverAttack(AttackObject incomingAttack)
+    {
+        bool isAbsorbing = false;
+        float affinityDamageMultiplier = 1;
 
+        switch (combatantStats.combatantAttributes[incomingAttack.attackSpell.spellType])
+        {
+            case CombatantScriptableObject.AttributeAffinity.Resist:
+                affinityDamageMultiplier = 0.5f;
+                break;
+            case CombatantScriptableObject.AttributeAffinity.Weak:
+                affinityDamageMultiplier = 1.5f;
+                break;
+            case CombatantScriptableObject.AttributeAffinity.Null:
+                Debug.Log("Attack Nullified");
+                return null;
+            case CombatantScriptableObject.AttributeAffinity.Absorb:
+                Debug.Log("Attack Absorbed");
+                isAbsorbing = true;
+                break;
+            case CombatantScriptableObject.AttributeAffinity.Repel:
+                Debug.Log("Attack Reflected");
+                // Initialiser prevents endless pingpong between combatants.
+                incomingAttack.isInitialiser = !incomingAttack.isInitialiser;
+                return incomingAttack.isInitialiser ? null : incomingAttack;
+        }
+
+        //Check for evasion
+        if(!isAbsorbing)
+        {
+            var agilityDiff = combatantStats.combatantBaseStats.agility - incomingAttack.attackerStats.agility;
+            var luckDiff = combatantStats.combatantBaseStats.luck - incomingAttack.attackerStats.luck;
+            var hitRate = Mathf.Clamp(incomingAttack.attackSpell.spellHitRate - agilityDiff / 2 - luckDiff / 4, 5, 100);
+
+            if (hitRate < Random.value * 100)
+            {
+                Debug.Log("Attack Dodged");
+                return null;
             }
         }
 
+        bool attackIsPhysical = incomingAttack.attackSpell.spellType == SpellScriptableObject.SpellType.Blunt || incomingAttack.attackSpell.spellType == SpellScriptableObject.SpellType.Sharp;
+        int attackStat = attackIsPhysical ? incomingAttack.attackerStats.strength : incomingAttack.attackerStats.magic;
+
+        float grossDamage = Mathf.Sqrt(incomingAttack.attackSpell.spellBaseDamage * Random.Range(0.95f, 1.05f)) * Mathf.Sqrt(attackStat);
+        float resultingDamageTaken = grossDamage * affinityDamageMultiplier / Mathf.Sqrt(combatantStats.combatantBaseStats.endurance * 4.5f);
+
+        localHP = Mathf.RoundToInt(Mathf.Clamp(localHP - resultingDamageTaken * (isAbsorbing ? -1 : 1), 0, combatantStats.combatantMaxHealth));
+
+        return null;
     }
 }
