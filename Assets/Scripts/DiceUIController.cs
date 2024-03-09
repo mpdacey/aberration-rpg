9216d7ab33689f376519ec44f45041b2b9de06ed
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-09 19:39:54 +1100

    Created DiceUIController.cs
    This script will handle the visible appearance of the breaking dice.

---
+++ Assets/Scripts/DiceUIController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceUIController : MonoBehaviour
+{
+    [SerializeField] Sprite[] diceFaces;
+    [SerializeField] SpriteRenderer diceFaceRenderer;
+    [SerializeField] float revealTime = 0.75f;
+    [SerializeField] int numOfRolls = 4;
+
+    public void CastFace(int chosenFace, int[] allDiceFaces)
+    {
+        ShowDice();
+        StartCoroutine(RollFace(chosenFace, allDiceFaces));
+    }
+
+    public void UpdateFace(int newValue)
+    {
+        if (newValue <= 0) {
+            HideDice();
+            return;
+        }
+
+        diceFaceRenderer.sprite = diceFaces[newValue - 1];
+    }
+
+    private void ShowDice()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    private void HideDice() 
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(false);
+    }
+
+    IEnumerator RollFace(int chosenFace, int[] allDiceFaces)
+    {
+        for(int i = 0; i < numOfRolls; i++)
+        {
+            diceFaceRenderer.sprite = diceFaces[allDiceFaces[Mathf.FloorToInt(Random.Range(0, allDiceFaces.Length - 0.01f))]];
+            yield return new WaitForSeconds(revealTime / numOfRolls);
+        }
+        diceFaceRenderer.sprite = diceFaces[chosenFace - 1];
+    }
+}
