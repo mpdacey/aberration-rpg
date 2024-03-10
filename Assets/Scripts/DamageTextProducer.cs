0fea4158536359d4bde4bc4553ff52fd5c5d26f3
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-10 20:16:05 +1100

    Split ProduceDamageText method in two.
    I plan to use this core functionality for other methods.

--- Assets/Scripts/DamageTextProducer.cs
+++ Assets/Scripts/DamageTextProducer.cs
@@ -5,18 +5,21 @@ public class DamageTextProducer : MonoBehaviour
 {
     public GameObject damagePrefab;
 
-    public void ProduceDamageText(int damage, Color textColour)
+    public void ProduceDamageText(int damage, Color textColour) =>
+        ProduceText($"{damage}HP", textColour);
+
+    private void ProduceText(string text, Color textColour)
     {
         var damageObject = Instantiate(damagePrefab, transform, true);
-        if(damageObject.transform.GetChild(0).TryGetComponent(out TextMeshProUGUI textMeshUI))
+        if (damageObject.transform.GetChild(0).TryGetComponent(out TextMeshProUGUI textMeshUI))
         {
             textMeshUI.color = textColour;
-            textMeshUI.text = $"{damage}HP";
+            textMeshUI.text = text;
         }
-        else if(damageObject.transform.GetChild(0).TryGetComponent(out TextMeshPro textMesh))
+        else if (damageObject.transform.GetChild(0).TryGetComponent(out TextMeshPro textMesh))
         {
             textMesh.color = textColour;
-            textMesh.text = $"{damage}HP";
+            textMesh.text = text;
         }
         damageObject.transform.parent = transform;
         damageObject.transform.localPosition = Vector3.zero;
