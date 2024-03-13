a7320978aa8690b524dbbad071aa5ecb5a1d22f0
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-13 20:27:35 +1100

    Formation selector will return a random formation

--- Assets/FormationSelector.cs
+++ Assets/FormationSelector.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class FormationSelector : MonoBehaviour
 {
+    [System.Serializable]
+    public struct FormationTier
+    {
+        public FormationScriptableObject[] formations;
+    }
 
     public FormationScriptableObject introFormation;
+    [SerializeField] FormationTier[] formationTiers;
     private bool hasFoughtBefore = false;
     
     public FormationScriptableObject GetFormation()
     {
-        return introFormation;
+        if (!hasFoughtBefore)
+        {
+            hasFoughtBefore = true;
+            return introFormation;
+        }
+
+        int tierRangeModifier = Random.Range(-5, 5)/2;
+        int tierIndex = Mathf.Clamp(GameController.CurrentLevel % 3 + tierRangeModifier, 0, formationTiers.Length - 1);
+        int randomEncounterIndex = Random.Range(0, formationTiers[tierIndex].formations.Length - 1);
+
+        return formationTiers[tierIndex].formations[randomEncounterIndex];
     }
 }
