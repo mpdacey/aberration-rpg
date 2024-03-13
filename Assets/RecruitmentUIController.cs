a3a9df916d67c0ad601d3ebaebfe0bb5005ba2aa
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-14 10:45:50 +1100

    Decline button is selected on default

--- Assets/RecruitmentUIController.cs
+++ Assets/RecruitmentUIController.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class RecruitmentUIController : MonoBehaviour
 {
     public TextMeshProUGUI leftOffer;
     public TextMeshProUGUI rightOffer;
+    public Button defaultOfferButton;
+
+    private void OnEnable()
+    {
+        defaultOfferButton.Select();
+    }
 
     public void SetOffers(string leftOfferText, string rightOfferText)
     {
         leftOffer.text = leftOfferText;
         rightOffer.text = rightOfferText;
+    }
+
+    private void Update()
+    {
+        GameObject selectedObject = EventSystem.current.currentSelectedGameObject;
+
+        if (selectedObject == null)
+            defaultOfferButton.Select();
     }
 }
