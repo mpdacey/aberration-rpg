de3e24a4390967dfdcde5c2af3b72f9631ddbcfc
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-03-18 14:04:16 +1100

    Further extended equipment capabilities

--- Assets/Scripts/ScriptableObjectSources/EquipmentScriptableObject.cs
+++ Assets/Scripts/ScriptableObjectSources/EquipmentScriptableObject.cs
@@ -11,6 +11,10 @@ public class EquipmentScriptableObject : ScriptableObject
     }
 
     public string equipmentName;
+    public int equipmentHP;
+    public int equipmentSP;
+    public EquipmentType equipmentType;
     public CombatantScriptableObject.CombatantStats equipmentStats;
+    public CombatantScriptableObject.AttributeAffinityDictionaryItem[] equipmentAffinties;
     public SpellScriptableObject[] equipmentSpells;
 }
