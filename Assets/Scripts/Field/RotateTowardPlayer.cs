dd975f7dfaf2373ef6a8d6254538ef5afc2a4578
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-05-13 11:15:08 +1000

    Created treasure prefab off of new goal prefab

--- Assets/Scripts/Field/RotateTowardPlayer.cs
+++ Assets/Scripts/Field/RotateTowardPlayer.cs
@@ -4,6 +4,12 @@ public class RotateTowardPlayer : MonoBehaviour
 {
     public Transform player;
 
+    private void Awake()
+    {
+        if (player == null)
+            player = GameObject.FindWithTag("Player").transform;
+    }
+
     void Update()
     {
         transform.rotation = player.rotation;
