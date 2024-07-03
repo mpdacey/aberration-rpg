cfa73f89fc44a9355d1d6965da920df582250661
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-07-03 18:16:41 +1000

    Created generic rift factory.
    From the ashes of the treasure rift factory comes a new factory that can create multiple kinds of rifts, each with there own customizable appearance rates.
    
    Also removed any trace of a treasure rift being mentioned as any rift can spawn where treasure rifts did spawn.

---
+++ Assets/Scripts/Field/RiftFactory.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiftFactory : MonoBehaviour
+{
+    [System.Serializable]
+    private struct RiftType
+    {
+        public GameObject riftPrefab;
+        [Range(0, 1)]
+        public float spawnChance;
+
+    }
+
+    public Transform riftContainer;
+    [SerializeField]
+    private RiftType[] rifts;
+
+    private void OnEnable()
+    {
+        LevelGenerator.RiftLocationsFound += GenerateTreasure;
+    }
+
+    private void OnDisable()
+    {
+        LevelGenerator.RiftLocationsFound -= GenerateTreasure;
+    }
+
+    private void GenerateTreasure(List<Vector2> possibleRiftPositions)
+    {
+        ClearRifts();
+
+        foreach(var riftPosition in possibleRiftPositions)
+        {
+            for(int i = 0; i < rifts.Length; i++)
+            {
+                if (Random.value > rifts[i].spawnChance)
+                    continue;
+
+                var rift = Instantiate(rifts[i].riftPrefab, riftContainer);
+                rift.transform.position = new Vector3(riftPosition.x * 5, 0, riftPosition.y * 5);
+            }
+        }
+    }
+
+    private void ClearRifts()
+    {
+        for (int i = 0; i < riftContainer.childCount; i++)
+            Destroy(riftContainer.GetChild(i).gameObject);
+    }
+}
