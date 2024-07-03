353259f97494ee7d077f7e44e4e03ed2b89e8411
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-07-03 17:36:02 +1000

    Created treasure rift controller
    The treasure rift controller to handle the responsibilities of calling the treasure found event and dismissal.

---
+++ Assets/Scripts/Field/TreasureRiftController.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TreasureRiftController : MonoBehaviour, IInteractable
+{
+    public static event Action TreasureFound;
+
+    public void Interact()
+    {
+        TreasureHandling();
+    }
+
+    private void OnDisable()
+    {
+        // If somehow the rift gets disabled while equipment UI is available
+        // this code should remove the event.
+        try
+        {
+            EquipmentController.DismissRift -= DismissRift;
+        }
+        catch { };
+    }
+
+    private void TreasureHandling()
+    {
+        EquipmentController.DismissRift += DismissRift;
+        if (TreasureFound != null)
+            TreasureFound.Invoke();
+    }
+
+    private void DismissRift()
+    {
+        EquipmentController.DismissRift -= DismissRift;
+        GetComponent<Animator>().Play("Dismiss");
+    }
+}
