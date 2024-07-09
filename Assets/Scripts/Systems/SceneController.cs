17119b116f36a324a387d223e5e3eb8e431f4f1a
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-07-09 16:31:39 +1000

    Added Manual Scene to SceneController

--- Assets/Scripts/Systems/SceneController.cs
+++ Assets/Scripts/Systems/SceneController.cs
@@ -2,23 +2,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System;
 using System.Collections;
-using System.Collections.Generic;
 
 public class SceneController : MonoBehaviour
 {
     public static event Action CombatSceneLoaded;
     public static event Action TitleSceneLoaded;
+    public static event Action ManualSceneLoaded;
+    public static event Action ManualSceneUnloaded;
 
     private const int COMBAT_SCENE_INDEX = 1;
     private const int TITLE_SCENE_INDEX = 2;
+    private const int MANUAL_SCENE_INDEX = 3;
 
     public IEnumerator LoadTitleScene()
     {
         UnloadScenes();
 
-        var asyncLoadLevel = SceneManager.LoadSceneAsync(TITLE_SCENE_INDEX, LoadSceneMode.Additive);
-        while (!asyncLoadLevel.isDone)
-            yield return new WaitForEndOfFrame();
+        yield return LoadScene(TITLE_SCENE_INDEX);
 
         if (TitleSceneLoaded != null)
             TitleSceneLoaded.Invoke();
@@ -28,20 +28,48 @@ public class SceneController : MonoBehaviour
     {
         UnloadScenes();
 
-        var asyncLoadLevel = SceneManager.LoadSceneAsync(COMBAT_SCENE_INDEX, LoadSceneMode.Additive);
-        while (!asyncLoadLevel.isDone)
-            yield return new WaitForEndOfFrame();
+        yield return LoadScene(COMBAT_SCENE_INDEX);
 
         if (CombatSceneLoaded != null)
             CombatSceneLoaded.Invoke();
     }
 
+    public IEnumerator LoadManualScene()
+    {
+        yield return LoadScene(MANUAL_SCENE_INDEX);
+
+        if (ManualSceneLoaded != null)
+            ManualSceneLoaded.Invoke();
+    }
+
+    public IEnumerator UnloadManualScene()
+    {
+        if (SceneManager.GetSceneByBuildIndex(MANUAL_SCENE_INDEX).isLoaded)
+        {
+            yield return SceneManager.UnloadSceneAsync(MANUAL_SCENE_INDEX);
+
+            if (ManualSceneUnloaded != null)
+                ManualSceneUnloaded.Invoke();
+        }
+    }
+
+    private IEnumerator LoadScene(int sceneIndex)
+    {
+        var asyncLoadLevel = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+        while (!asyncLoadLevel.isDone)
+            yield return new WaitForEndOfFrame();
+    }
+
     private void UnloadScenes()
     {
-        if (SceneManager.GetSceneByBuildIndex(COMBAT_SCENE_INDEX).isLoaded)
-            SceneManager.UnloadSceneAsync(COMBAT_SCENE_INDEX);
+        UnloadScene(COMBAT_SCENE_INDEX);
+        UnloadScene(TITLE_SCENE_INDEX);
+        UnloadScene(MANUAL_SCENE_INDEX);
+    }
 
-        if (SceneManager.GetSceneByBuildIndex(TITLE_SCENE_INDEX).isLoaded)
-            SceneManager.UnloadSceneAsync(TITLE_SCENE_INDEX);
+    private void UnloadScene(int sceneIndex)
+    {
+        if (SceneManager.GetSceneByBuildIndex(sceneIndex).isLoaded)
+            SceneManager.UnloadSceneAsync(sceneIndex);
     }
 }
