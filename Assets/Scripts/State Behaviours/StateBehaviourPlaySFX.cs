6b7378c61f16e92453cdb76dd7a53deb4688995c
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-07-22 17:49:18 +1000

    Created state behaviour to play the manual sound effects.

---
+++ Assets/Scripts/State Behaviours/StateBehaviourPlaySFX.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class StateBehaviourPlaySFX : StateMachineBehaviour
+{
+    public AudioClip sfx;
+    public bool randomPitch;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (sfx != null)
+            AudioManager.PlayAudioClip(sfx, randomPitch);
+    }
+}
+
