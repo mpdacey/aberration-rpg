4907eea43bfca24b33b262368173d8f31efb02f8
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-08-02 12:04:21 +1000

    Last selected is set a frame later.
    After selecting an option in a menu and advance into the next menu, last selected will still be saved as the previous menu item. This delay will now force last selected to be set to the menu's item, while still maintaining the functionality of preventing move sound spam.

--- Assets/Scripts/Systems/MenuSoundEventTriggerListener.cs
+++ Assets/Scripts/Systems/MenuSoundEventTriggerListener.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Cryptemental.Audio;
@@ -18,22 +19,33 @@ public class MenuSoundEventTriggerListener : MonoBehaviour
         if (currentSystem.currentSelectedGameObject == lastSelected) return;
 
         AudioManager.PlayAudioClip(menuMoveClip);
-        lastSelected = currentSystem.currentSelectedGameObject;
+        StartCoroutine(SetLastSelected());
     }
 
     public void OnButtonSubmit()
     {
-        if(!isInspecting)
-            AudioManager.PlayAudioClip(menuConfirmClip);
+        if (isInspecting) return;
+        AudioManager.PlayAudioClip(menuConfirmClip);
+        StartCoroutine(SetLastSelected());
     }
 
     public void OnButtonCancel()
     {
         AudioManager.PlayAudioClip(menuCancelClip);
+        StartCoroutine(SetLastSelected());
     }
 
     public void OnSliderValueChange()
     {
         AudioManager.PlayAudioClip(menuMoveClip);
+    }
+
+    IEnumerator SetLastSelected()
+    {
+        yield return new WaitForEndOfFrame();
+
+        EventSystem currentSystem = EventSystem.current;
+        if (currentSystem && currentSystem.currentSelectedGameObject != lastSelected)
+            lastSelected = currentSystem.currentSelectedGameObject;
     }
 }
